0c5f200211b5ad3f960a3a83ff84a029ead982c1
janusz <janusz@biggerpicture.agency>
2019-09-26 09:20:30 +0200

    init commit

---
+++ php-fpm/scripts/install_software.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+apt-get update -y && apt-get install -y \
+    curl \
+    dnsutils \
+    gdb \
+    git \
+    htop \
+    iputils-ping \
+    ltrace \
+    make \
+    procps \
+    strace \
+    sudo \
+    sysstat \
+    unzip \
+    nano \
+    wget
