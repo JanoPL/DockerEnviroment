6d7d7e07cbd80b764dec90c1bb97b284739113cf
JanoPL <xjanusz4@gmail.com>
2019-09-28 19:08:41 +0200

    Move files to Dir

---
+++ Docker/php-fpm/scripts/install_software.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+apt-get update -y && apt-get install -y \
+    curl \
+    dnsutils \
+    gdb \
+    iputils-ping \
+    ltrace \
+    make \
+    procps \
+    strace \
+    sudo \
+    sysstat \
+    unzip \
+    nano \
+    wget
