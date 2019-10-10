cbd72d3495a4ca69f7baa69df235df6d4decbe93
JanoPL <xjanusz4@gmail.com>
2019-10-10 09:39:50 +0200

    Changes fpm install script

--- Docker/php-fpm/scripts/php_extension_install.sh
+++ Docker/php-fpm/scripts/php_extension_install.sh
@@ -4,13 +4,13 @@
 apt-get update -yqq && apt-get -f install -yyq wget
 
 FILE=/tmp/scripts/install-php-extensions.sh
-if ! [ -f "$FILE" ]; then
+if [ -f "$FILE" ]; then
+  cp /tmp/scripts/install-php-extensions.sh /usr/local/bin/install-php-extensions
+else
 # download helper script
 # @see https://github.com/mlocati/docker-php-extension-installer/
 wget -q -O /usr/local/bin/install-php-extensions https://raw.githubusercontent.com/mlocati/docker-php-extension-installer/master/install-php-extensions \
     || (echo "Failed while downloading php extension installer!"; exit 1)
-else
-  cp $FILE /usr/local/bin/install-php-extensions
 fi
 # install extensions
 chmod uga+x /usr/local/bin/install-php-extensions && sync && install-php-extensions \
