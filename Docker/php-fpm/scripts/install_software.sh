19892eefcbf57fb7a208aac4c225f441355c4607
janusz <xjanusz4@gmail.com>
2019-10-15 10:12:45 +0200

    Removed unnecessary software, change xdebug file

--- Docker/php-fpm/scripts/install_software.sh
+++ Docker/php-fpm/scripts/install_software.sh
@@ -3,14 +3,12 @@ apt-get update -y && apt-get install -y \
     curl \
     dnsutils \
     gdb \
-    iputils-ping \
     ltrace \
     make \
     procps \
     strace \
     sudo \
     sysstat \
-    unzip \
     nano \
     wget \
     bash
