ad0768a114a10d37603291c328024a50b3be832b
JanoPL <janusz@biggerpicture.agency>
2019-11-15 13:41:37 +0100

    change to download the script

--- Docker/php-fpm/scripts/php_extension_install.sh
+++ Docker/php-fpm/scripts/php_extension_install.sh
@@ -4,14 +4,17 @@
 apt-get update -yqq && apt-get -f install -yyq wget
 
 FILE=/tmp/scripts/install-php-extensions.sh
-if [ -f "$FILE" ]; then
-  cp /tmp/scripts/install-php-extensions.sh /usr/local/bin/install-php-extensions
+
+# Check the online script
+status="$(curl -Is https://raw.githubusercontent.com/mlocati/docker-php-extension-installer/master/install-php-extensions | head -1)";
+validate=($status);
+if [ "${validate[-2]}" == "200" ]; then
+  wget -q -O /usr/local/bin/install-php-extensions https://raw.githubusercontent.com/mlocati/docker-php-extension-installer/master/install-php-extensions \
+      || (echo "Failed while downloading php extension installer!"; exit 1);
 else
-# download helper script
-# @see https://github.com/mlocati/docker-php-extension-installer/
-wget -q -O /usr/local/bin/install-php-extensions https://raw.githubusercontent.com/mlocati/docker-php-extension-installer/master/install-php-extensions \
-    || (echo "Failed while downloading php extension installer!"; exit 1)
+  cp /tmp/scripts/install-php-extensions.sh /usr/local/bin/install-php-extensions
 fi
+
 # install extensions
 chmod uga+x /usr/local/bin/install-php-extensions && sync && install-php-extensions \
     opcache \
